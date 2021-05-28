d3fa05fb9d7f791cf5d14760dfb36ff9fde277d2
lacj@kea.dk <lacj@kea.dk>
2021-05-28 11:43:11 +0200

    and so it begins

---
+++ main.js
@@ -0,0 +1,12 @@
+import "./main.css";
+import "./style.scss";
+
+/* import lacjimg from "./sonofnature.jpg";
+
+document.querySelector("img").src = lacjimg; */
+
+document.querySelector("img").src = "sonofnature.jpg";
+
+document.querySelector("#app").innerHTML = `
+<h1>Hello Vite</h1>
+<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
